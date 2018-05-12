9b242efa9971600f691f4e97b924db14fbf64142
Lee Cooper <lee.cooper@gmail.com>
2018-05-12 12:42:01 -0700

    Lecture 2

---
+++ Concentration/Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Lee Cooper on 5/12/18.
+//  Copyright © 2018 Lee Cooper. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
